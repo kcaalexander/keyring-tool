fed75375aed8f41ae6abb65375cdb55763df8d61
Alexander Thomas <alexander@collab.net>
2011-12-26 09:23:50 +0000

    Fixing broken autogen.sh by changing C-style comments to shell-style comments.
    * autogen.sh
       Fixed broken comments.

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,22 @@
-/*
-   Copyright (C) 2011, Alexander Thomas
-
-   This file is part of the Gnome Keyring Command Line Tool.
-
-   This is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this tool.  If not, see <http://www.gnu.org/licenses/>.
-
-   Author: Alexander Thomas <alexander@collab.net>
-*/
+##
+## Copyright (C) 2011, Alexander Thomas
+##
+## This file is part of the Gnome Keyring Command Line Tool.
+##
+## This is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this tool.  If not, see <http://www.gnu.org/licenses/>.
+##
+## Author: Alexander Thomas <alexander@collab.net>
 
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
